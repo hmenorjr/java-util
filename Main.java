6b705b79b631c1a87889b6e4b35c854491b2ddf4
hmenorjr <herman.menor@gmail.com>
2019-12-13 15:22:15 +0800

    Test for new classes

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-// import Numeric.*;
-// import String.*;
+
+import Numeric.Numeric;
+import String.Roman;
 // add more folders as you like
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Roman roman = new Roman();
+        Numeric numeric = new Numeric();
+
+        for (int i = 1; i <= 50; i++) {
+            String romanVersion = numeric.numericToRoman(i);
+            int intVersion = roman.romanToNumeric(romanVersion);
+            System.out.println("Roman " + romanVersion + " is " + intVersion);
+        }
     }
 }
